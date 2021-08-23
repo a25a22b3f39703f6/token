437dcc2ae272a358775cc5cc573e89bc9c4d3c1b
a25a22b3f39703f6 <1940265424@qq.com>
2021-08-23 16:48:31 +0800

    ?

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,15 @@
  * @version:
  * @Author: CK
  * @Date: 2021-06-06 20:40:04
- * @LastEditors: CK
- * @LastEditTime: 2021-08-23 15:38:18
+ * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2021-08-23 16:46:01
 -->
 <template>
   <div id="nav">
     <router-link to="/">Home</router-link> |
-    <router-link to="/about" :md5="'23'">About</router-link>
+    <router-link to="/about" @click="startToken()">About</router-link>
   </div>
-  <router-view />
+  <router-view :md5="tokenStr"/>
 </template>
 
 <style>
@@ -39,9 +39,31 @@
 
 <script>
 import md5 from 'js-md5'
-import genToken from 'tools/token'
+import { genToken } from './tools/token'
 export default {
-  name: 'App'
+  name: 'App',
+  data () {
+    return {
+      yz: '123',
+      tokenStr: '',
+      tokenNum: 0
+    }
+  },
+  methods: {
+    /**
+     * @test: 启动令牌生成计时器
+     */
+    startToken () {
+      const tokenObj = genToken(this.yz, md5)
+      this.tokenStr = tokenObj.token
+      this.tokenNum = setTimeout(this.startToken, tokenObj.time)
+    },
+    /**
+     * @test: 关闭令牌生成计时器
+     */
+    endToken () {
+      clearTimeout(this.tokenNum)
+    }
+  }
 }
-console.log(genToken)
 </script>
--- src/tools/token.js
+++ src/tools/token.js
@@ -3,35 +3,41 @@
  * @version:
  * @Author: CK
  * @Date: 2021-08-23 15:25:32
- * @LastEditors: CK
- * @LastEditTime: 2021-08-23 15:42:43
+ * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2021-08-23 16:48:08
  */
 /**
  * @test: 根据盐值和当前时间戳生成纯数字验证码
  * @param {String} salt 盐值
+ * @param {Function} coding 编码函数
  * @param {Number} [num = 0] 模拟误差30s次数
- * @param {Number} [num = 0] 模拟误差30s次数
- * @return {String}
+ * @return {Object} [token, time] token:验证码 time:距离下一次30s的毫秒数
  */
 export function genToken (salt, coding, num = 0) {
   const date = Date.now()
   const minute = Math.floor(date / 30000) + num
   const tokenStr = coding(minute + salt)
   const __token = parseInt(tokenStr, 16) % 1000000 + ''
-  return __token.padStart(6, '0')
+  const token = __token.padStart(6, '0')
+  const time = 30000 - date % 30000
+  return {
+    token,
+    time
+  }
 }
 /**
  * @test: 根据盐值和验证码判断是否正确
  * @param {String} salt 盐值
- * @param {String} __token
+ * @param {String} token 验证码
+ * * @param {String} coding 编码函数
  * @param {Number} [errorValue = 4] 误差30s次数范围
  * @return {Boolean}
  */
- export function isToken (salt, __token, coding, errorValue = 4) {
+export function isToken (salt, token, coding, errorValue = 4) {
   let __errorValue = -errorValue - 1
   const maxErrorValue = errorValue
   while (__errorValue++ !== maxErrorValue) {
-    if (genToken(salt, coding, __errorValue) === __token) {
+    if (genToken(salt, coding, __errorValue) === token) {
       return true
     }
   }
@@ -39,6 +45,6 @@ export function genToken (salt, coding, num = 0) {
 }
 
 export default {
-    genToken,
-    isToken
+  genToken,
+  isToken
 }
