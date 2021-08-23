78b851621e3986db776c9e072610d9fd3f09c8bb
a25a22b3f39703f6 <1940265424@qq.com>
2021-08-23 14:00:02 +0800

    更新 vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,8 @@
-const ENV = process.env.NODE_ENV;
+const ENV = process.env.NODE_ENV
 
 module.exports = {
   devServer: {
     disableHostCheck: true
   },
-  publicPath: ENV === "development" ? "" : "/token/", // 关键代码   token就是仓库名
+  publicPath: ENV === 'development' ? '' : '/token/' // 关键代码   token就是仓库名
 }
