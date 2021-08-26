384f3a5765fef4068ca0d0075ae5884e1f5168cf
a25a22b3f39703f6 <1940265424@qq.com>
2021-08-26 22:48:43 +0800

    更新 About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,17 @@
+<!--
+ * @Author: your name
+ * @Date: 2021-06-06 20:40:04
+ * @LastEditTime: 2021-08-26 22:48:40
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: \token\src\views\About.vue
+-->
 <template>
   <div class="about">
     <h1>令牌:{{md5}}</h1>
+    <p>
+      23
+    </p>
   </div>
 </template>
 
